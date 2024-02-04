12857bc128d240866790ba663dfa0a67194a486b
james <131738190+pipersjames@users.noreply.github.com>
2024-02-04 17:08:55 +1000

    Model: Auth model and User revamp
    added model for authorization options and adjusted the User model to reflect the wireframe

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const {User} = require('../models/UserModel');
 const {comparePassword, generateJwt} = require('../utils/userAuthFunctions');
+const { Collection } = require("mongoose");
 
 const router = express.Router();
 
@@ -14,4 +15,15 @@ return null
 // POST
 router.post("/signup", async (request, response) =>{
     return null
+})
+
+router.post("/login", async (request, response) => {
+    try {
+        const check = await Collection.findOne({email: request.body.email})
+        if (!check) {
+            response.send("email not found")
+        }  
+    } catch(e) {
+        console.warn(e)
+    }
 })
